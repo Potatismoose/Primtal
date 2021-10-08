e701744f89074edfbaf6b24dae2c9a1e6cd6a909
Benny Christensen <benny@potatismoose.com>
2021-10-08 12:39:19 +0200

    Add project files.

---
+++ Primtal/Menu.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Primtal
+{
+    static class Menu
+    {
+        /// <summary>
+        /// Runs the program and prints the menu
+        /// </summary>
+        internal static void Run()
+        {
+            //instance of PrimeNumber
+            PrimeNumber pm = new();
+            var errorMsg = "";
+            var endProgram = false;
+            //Menu items
+            string[] mainMenuItems = { "Kontrollera om ett tal är ett Primtal", "lägg till nästa primtal", "Lista alla sparade primtal", "Avsluta programmet" };
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"\t{errorMsg}");
+
+                //Prints menu
+                for (int i = 0; i < mainMenuItems.Length; i++)
+                {
+                    Console.WriteLine($"\t{i + 1}. {mainMenuItems[i]}");
+                }
+                Console.Write("\n\tGör ditt val /> ");
+                //Checks if number been pressed
+                var successfullyConverted = int.TryParse(Console.ReadLine(), out int choice);
+
+                errorMsg = default;
+                int number;
+
+                //Switch for menu choice
+                switch (choice)
+                {
+                    //If 0 pressed or if string is typed in
+                    case 0:
+                        if (successfullyConverted)
+                        {
+                            errorMsg = "Felaktigt menyval";
+                        }
+                        else
+                        {
+                            errorMsg = "Felaktig inmatning, endast nummer tillåna";
+                        }
+                        break;
+                    case 1:
+                        //Recieves validated input from user
+                        number = InputNumber();
+                        //Calculates if number is Prime number
+                        var isPrimenumber = pm.Calculate(number);
+
+                        //If number is primenumber
+                        if (isPrimenumber)
+                        {
+                            Console.WriteLine($"\tTalet {number} är ett primtal. Om det inte sparats tidigare, så ligger det nu i din lista.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\tTalet {number} är inget primtal.");
+                        }
+                        Console.WriteLine("\tTryck enter för att fortsätta.");
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        //Calculate the next prime number
+                        number = pm.CalculateNextPrimeNumber();
+                        errorMsg = $"\n\tNästa Primtal, ({number}), har lagts till i listan";
+                        break;
+                    case 3:
+                        //Lists all prime numbers added to the list
+                        pm.ListPrimeNumbers();
+                        break;
+                    case 4:
+                        //Quit program
+                        endProgram = true;
+                        break;
+                    default:
+                        //Default error message if user presses number above number of menu options in the menu.
+                        errorMsg = "Felaktigt menyval";
+                        break;
+                }
+            } while (!endProgram);
+        }
+
+        /// <summary>
+        /// Takes a input from the user, and validates so it's above 0 and not a string.
+        /// </summary>
+        /// <returns>Returns valid integer (above 0).</returns>
+        private static int InputNumber()
+        {
+            var errorMsg = default(string);
+            int input;
+            var wrongInput = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n\tPrimtal?");
+                Console.WriteLine("\t" + errorMsg);
+                Console.Write("\tKontrollera det här talet/> ");
+                var isNumber = int.TryParse(Console.ReadLine(), out input);
+                if (isNumber && input > 0)
+                {
+                    wrongInput = false;
+                    errorMsg = default;
+                }
+                else if (input < 1 && isNumber)
+                {
+                    errorMsg = "Du behöver mata in ett tal större än 0";
+                }
+                else
+                {
+                    errorMsg = "Felaktig inmatning, endast heltal är giltiga";
+                }
+
+            } while (wrongInput);
+
+            return input;
+        }
+    }
+}
---
+++ Primtal/PrimeNumber.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Primtal
+{
+    class PrimeNumber
+    {
+        /// <summary>
+        /// List with users primenumbers
+        /// </summary>
+        SortedList<int, int> primeNumbers = new();
+
+        /// <summary>
+        /// Calculates if the number is a primenumber
+        /// </summary>
+        /// <param name="number">Takes a user input as a number to check for prime number validity</param>
+        /// <returns>Returns true if number is a prime number, false if not</returns>
+        internal bool Calculate(int number)
+        {
+            //test for edgecase if number is 0 or less
+            if (number <= 1)
+            {
+                return false;
+            }
+            //Check if number is a even number but not 2
+            else if (number % 2 == 0 && number !=2)
+            {
+                return false;
+            }
+            //Takes the square root from the number and checks
+            //dividability with all numbers from 2 up til result from square root.
+            //If dividable with a number, return false.
+            //Else check if number already been added and add number if it does not already exists in list
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            if (!primeNumbers.ContainsKey(number))
+            {
+                primeNumbers.Add(number, number);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Simple method for printing out the list if it contains any items.
+        /// Else print a message saying the list is empty.
+        /// </summary>
+        internal void ListPrimeNumbers()
+        {
+            Console.WriteLine();
+            
+            if (primeNumbers.Count > 0)
+            {
+                Console.WriteLine("\tHär är de primtal du angett eller genererat:");
+                foreach (var item in primeNumbers)
+                {
+                    Console.WriteLine("\t" + item.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\tInga nummer finns i listan");
+            }
+
+            Console.WriteLine("\n\tTryck valfri knapp för att fortsätta...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Method that takes the last integer in the sorted list and calculates the next prime number
+        /// </summary>
+        /// <returns>Returns the next integer that is a prime number</returns>
+        internal int CalculateNextPrimeNumber(){
+            int highestNumber;
+            if (primeNumbers.Count == 0){
+                highestNumber = default;
+            }
+            else{
+                //Takes the last number (value) of the sorted list and assigns it value to highestNumber
+                 highestNumber = primeNumbers.Values[^1];
+            }
+            //Loop that runs until it finds the next prime number.
+            while (true)
+            {
+                if (Calculate(++highestNumber))
+                {
+                    return highestNumber;
+                }
+            }
+
+        }
+    }
+}
